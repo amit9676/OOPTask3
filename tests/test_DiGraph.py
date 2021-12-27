04ea7ad3f5f2e8da5243a1667c8236485b7704e7
yehonatan169 <yehonatanamosi@gmail.com>
2021-12-27 20:37:40 +0200

    new update

---
+++ tests/test_DiGraph.py
@@ -0,0 +1,168 @@
+from unittest import TestCase
+from src.DiGraph import DiGraph
+
+
+# noinspection DuplicatedCode
+class TestDiGraph(TestCase):
+
+    # def test_get_node(self):
+    #     self.fail()
+    #
+    # def test_get_edge(self):
+    #     self.fail()
+
+    def test_v_size(self):
+        g = DiGraph()
+        self.assertEqual(g.v_size(), 0)
+        g.add_node(1)
+        g.add_node(2)
+        g.add_node(10)
+        self.assertEqual(g.v_size(), 3)
+
+    def test_e_size(self):
+        g = DiGraph()
+        g.add_node(node_id=1)
+        g.add_node(node_id=2)
+        self.assertEqual(0, g.e_size())
+        g.add_edge(id1=1, id2=2, weight=4)
+        self.assertEqual(1, g.e_size())
+
+    def test_get_all_v(self):
+        g = DiGraph()
+        g.add_node(node_id=1)
+        g.add_node(node_id=2)
+        g.add_node(node_id=3)
+        self.assertEqual({1, 2, 3}, g.get_all_v().keys())
+
+    def test_all_in_edges_of_node(self):
+        g1 = DiGraph()
+        g1.add_node(node_id=1)
+        g1.add_node(node_id=2)
+        g1.add_node(node_id=3)
+        g1.add_edge(2, 1, 6)
+        g1.add_edge(1, 3, 4)
+        g1.add_edge(3, 2, 7)
+        self.assertEqual(6, g1.all_in_edges_of_node(1).get(2))
+        self.assertEqual(7, g1.all_in_edges_of_node(2).get(3))
+        self.assertEqual(4, g1.all_in_edges_of_node(3).get(1))
+
+        g2 = DiGraph()
+        g2.add_node(node_id=1)
+        g2.add_node(node_id=2)
+        g2.add_node(node_id=3)
+        g2.add_node(node_id=4)
+        g2.add_node(node_id=5)
+        g2.add_edge(1, 2, 1)
+        g2.add_edge(1, 3, 2)
+        g2.add_edge(1, 4, 3)
+        g2.add_edge(1, 5, 4)
+        self.assertEqual({}, g2.all_in_edges_of_node(1))
+        self.assertEqual(1, g2.all_in_edges_of_node(2).get(1))
+        self.assertEqual(2, g2.all_in_edges_of_node(3).get(1))
+        self.assertEqual(3, g2.all_in_edges_of_node(4).get(1))
+        self.assertEqual(4, g2.all_in_edges_of_node(5).get(1))
+
+    def test_all_out_edges_of_node(self):
+        g3 = DiGraph()
+        g3.add_node(node_id=1)
+        g3.add_node(node_id=2)
+        g3.add_node(node_id=3)
+        g3.add_edge(1, 2, 6)
+        g3.add_edge(1, 3, 7)
+        self.assertEqual(6, g3.all_out_edges_of_node(1).get(2))
+        self.assertEqual(7, g3.all_out_edges_of_node(1).get(3))
+
+        g4 = DiGraph()
+        g4.add_node(node_id=1)
+        g4.add_node(node_id=2)
+        g4.add_node(node_id=3)
+        g4.add_node(node_id=4)
+        g4.add_node(node_id=5)
+        g4.add_edge(1, 2, 1)
+        g4.add_edge(1, 3, 2)
+        g4.add_edge(1, 4, 3)
+        g4.add_edge(1, 5, 4)
+        self.assertEqual(1, g4.all_in_edges_of_node(2).get(1))
+        self.assertEqual(2, g4.all_in_edges_of_node(3).get(1))
+        self.assertEqual(3, g4.all_in_edges_of_node(4).get(1))
+        self.assertEqual(4, g4.all_in_edges_of_node(5).get(1))
+
+    def test_get_mc(self):
+        g = DiGraph()
+        g.add_node(1, (1, 2, 0))
+        g.add_node(2, (1, 5, 0))
+        g.add_node(3, (5, 1, 0))
+        g.add_edge(1, 2, 6)
+        g.add_edge(1, 3, 7)
+        g.add_edge(2, 3, 1.33)
+        self.assertEqual(g.get_mc(), 6)
+
+    def test_add_edge(self):
+        g5 = DiGraph()
+        g5.add_node(node_id=1)
+        g5.add_node(node_id=2)
+        g5.add_node(node_id=3)
+        g5.add_edge(1, 2, 6)
+        g5.add_edge(1, 3, 7)
+
+        numin = 0
+        print(g5.neighborsIn)
+        print(g5.neighborsIn.values())
+        for di in g5.neighborsIn:
+            numin += len(g5.neighborsIn[di])
+        self.assertEqual(2, numin)
+
+        num_out = 0
+        for di in g5.neighborsOut:
+            num_out += len(g5.neighborsOut[di])
+        self.assertEqual(2, num_out)
+
+        g6 = DiGraph()
+        g6.add_edge(0, 1, 1.33)
+        self.assertEqual(g6.e_size(), 0)
+        g6.add_node(0)
+        g6.add_node(1, (1, 2, 0))
+        g6.add_edge(0, 1, 12)
+        g6.add_edge(0, 1, 12)
+        g6.add_edge(1, 0, 12)
+        g6.add_edge(1, 20, 12)
+        self.assertEqual(g6.e_size(), 2)
+
+    def test_add_node(self):
+        g = DiGraph()
+        self.assertEqual(g.v_size(), 0)
+        g.add_node(1)
+        g.add_node(2)
+        g.add_node(10)
+        self.assertFalse(g.add_node(10))
+        self.assertEqual(g.v_size(), 3)
+        self.assertEqual(1, g.vertices.get(1).get_key())
+
+    def test_remove_node(self):
+        g8 = DiGraph()
+        g8.add_node(node_id=1)
+        g8.add_node(node_id=2)
+        g8.add_node(node_id=3)
+        g8.add_node(node_id=4)
+        g8.add_node(node_id=5)
+        g8.add_node(node_id=6)
+        self.assertEqual(6, g8.v_size())
+        g8.remove_node(1)
+        g8.remove_node(3)
+        g8.remove_node(5)
+        self.assertEqual(3, g8.v_size())
+
+    def test_remove_edge(self):
+        g9 = DiGraph()
+        g9.add_node(node_id=1)
+        g9.add_node(node_id=2)
+        g9.add_node(node_id=3)
+        g9.add_node(node_id=4)
+        g9.add_edge(1, 2, 32)
+        g9.add_edge(2, 3, 32)
+        g9.add_edge(3, 4, 32)
+        self.assertEqual(3, g9.e_size())
+
+        g9.remove_edge(3, 4)
+        g9.remove_edge(2, 3)
+        self.assertEqual(1, g9.e_size())
---
+++ tests/test_GraphAlgo.py
@@ -0,0 +1,175 @@
+import math
+from unittest import TestCase
+from src.DiGraph import DiGraph
+from src.GraphAlgo import GraphAlgo
+
+
+class TestGraphAlgo(TestCase):
+
+    def graph1(self) -> DiGraph:
+        g1 = DiGraph()  # creates an empty directed graph
+        for n in range(3):
+            g1.add_node(n)
+
+        g1.add_edge(0, 1, 3)
+        g1.add_edge(1, 2, 4)
+        g1.add_edge(2, 0, 5)
+
+        return g1
+
+    def graph2(self):
+        g2 = DiGraph()  # creates an empty directed graph
+        for n in range(8):
+            g2.add_node(n)
+
+        g2.add_edge(0, 1, 10)
+        g2.add_edge(1, 5, 8)
+        g2.add_edge(2, 1, 2)
+        g2.add_edge(3, 2, 2)
+        g2.add_edge(3, 1, 5)
+        g2.add_edge(4, 0, 11)
+        g2.add_edge(1, 4, 4)
+        g2.add_edge(7, 4, 4)
+        g2.add_edge(5, 6, 9)
+
+        return g2
+
+    def graph3(self):
+        g3 = DiGraph()  # creates an empty directed graph
+        for n in range(14):
+            g3.add_node(n)
+
+        g3.add_edge(0, 3, 4)
+        g3.add_edge(1, 0, 6)
+        g3.add_edge(1, 5, 20)
+        g3.add_edge(1, 8, 19)
+        g3.add_edge(1, 11, 25)
+        g3.add_edge(2, 13, 11)
+        g3.add_edge(3, 1, 11)
+        #
+        g3.add_edge(3, 10, 17)
+        #
+        g3.add_edge(4, 1, 20)
+        g3.add_edge(4, 5, 6)
+        g3.add_edge(5, 4, 4)
+        #
+        g3.add_edge(6, 1, 5)
+        #
+        g3.add_edge(6, 7, 8)
+        g3.add_edge(7, 6, 8)
+        #
+        g3.add_edge(8, 7, 15)
+        #
+        g3.add_edge(9, 11, 10)
+        g3.add_edge(9, 12, 7)
+        g3.add_edge(10, 6, 5)
+        g3.add_edge(10, 9, 6)
+        g3.add_edge(11, 13, 23)
+        g3.add_edge(12, 2, 10)
+        g3.add_edge(13, 8, 29)
+
+        return g3
+
+    def test_get_graph(self):
+        g = DiGraph()
+        ga = GraphAlgo()
+        self.assertIsNone(ga.get_graph())
+        ga = GraphAlgo(g)
+        self.assertEqual(ga.get_graph().get_all_v(), g.get_all_v())
+
+    def test_save_and_load_from_json(self):
+        ga = GraphAlgo()
+        ga.load_from_json("../data/A2.json")
+
+        ga1 = GraphAlgo(ga.get_graph())
+        self.assertEqual(ga1.get_graph().get_all_v(), ga.get_graph().get_all_v())
+
+        ga1.save_to_json("../data/newA2.json")
+        ga.load_from_json("../data/newA2.json")
+        self.assertEqual(ga1.get_graph().get_mc(), ga.get_graph().get_mc())
+        self.assertEqual(ga1.get_graph().v_size(), ga.get_graph().v_size())
+        self.assertEqual(ga1.get_graph().e_size(), ga.get_graph().e_size())
+
+    def test_is_connected(self):
+        g = GraphAlgo(self.graph1())
+        self.assertEqual(g.is_connected(), True)
+        g = GraphAlgo(self.graph2())
+        self.assertEqual(g.is_connected(), False)
+        g = GraphAlgo(self.graph3())
+        self.assertEqual(g.is_connected(), True)
+
+    def test_shortest_path(self):
+
+
+        g = GraphAlgo(self.graph1())
+        self.assertEqual(g.shortest_path(0, 2), (7, [0, 1, 2]))
+        self.assertEqual(g.shortest_path(1, 2), (4, [1, 2]))
+        self.assertEqual(g.shortest_path(2, 2), (0, [2]))
+        self.assertEqual(g.shortest_path(0, 1), (3, [0, 1]))
+        self.assertEqual(g.shortest_path(1, 1), (0, [1]))
+        self.assertEqual(g.shortest_path(2, 1), (8, [2, 0, 1]))
+        self.assertEqual(g.shortest_path(0, 0), (0, [0]))
+        self.assertEqual(g.shortest_path(1, 0), (9, [1, 2, 0]))
+        self.assertEqual(g.shortest_path(2, 0), (5, [2, 0]))
+
+        g = GraphAlgo(self.graph2())
+        self.assertEqual(g.shortest_path(0, 4), (14, [0, 1, 4]))
+        self.assertEqual(g.shortest_path(1, 6), (17, [1, 5, 6]))
+        self.assertEqual(g.shortest_path(5, 2), (math.inf, []))
+        self.assertEqual(g.shortest_path(3, 4), (8, [3, 2, 1, 4]))
+        self.assertEqual(g.shortest_path(2, 5), (10, [2, 1, 5]))
+        self.assertEqual(g.shortest_path(6, 4), (math.inf, []))
+
+        g = GraphAlgo(self.graph3())
+        self.assertEqual(g.shortest_path(0, 7), (34, [0, 3, 10, 6, 7]))
+        self.assertEqual(g.shortest_path(7, 12), (53, [7, 6, 1, 0, 3, 10, 9, 12]))
+        self.assertEqual(g.shortest_path(13, 6), (52, [13, 8, 7, 6]))
+        self.assertEqual(g.shortest_path(4, 5), (6, [4, 5]))
+        self.assertEqual(g.shortest_path(2, 0), (74, [2, 13, 8, 7, 6, 1, 0]))
+        self.assertEqual(g.shortest_path(9, 10), (112, [9, 12, 2, 13, 8, 7, 6, 1, 0, 3, 10]))
+        self.assertEqual(g.shortest_path(10, 4), (34, [10, 6, 1, 5, 4]))
+
+    def test_tsp(self):
+        g = GraphAlgo(self.graph1())
+        self.assertEqual(g.TSP([0, 2]), ([2, 0], 5))
+        g = GraphAlgo(self.graph2())
+        self.assertEqual(g.TSP([0, 2]), ([2, 1, 4, 0], 17))
+        self.assertEqual(g.TSP([1, 3, 6, 2]), ([3, 2, 1, 5, 6], 21))
+        g = GraphAlgo(self.graph3())
+        self.assertEqual(g.TSP([9, 7, 11, 5]), ([7, 6, 1, 5, 4, 1, 0, 3, 10, 9, 11], 100))
+        self.assertEqual(g.TSP([0, 1, 10, 2, 8, 4]), ([10, 6, 1, 5, 4, 1, 0, 3, 10, 9, 12, 2, 13, 8], 144))
+
+    def test_center_point(self):
+        g = GraphAlgo(self.graph1())
+        self.assertEqual(g.centerPoint(), (0, 7))
+        g = GraphAlgo(self.graph2())
+        self.assertEqual(g.centerPoint(), (None, float("inf")))
+        g = GraphAlgo(self.graph3())
+        self.assertEqual(g.centerPoint(), (10, 34))
+
+    def test_plot_graph(self):
+
+        g = GraphAlgo()
+        file0 = "../data/A0"
+        file1 = "../data/A1"
+        file2 = "../data/A2"
+        file3 = "../data/A3"
+        file4 = "../data/A4"
+        file5 = "../data/A5"
+        fileT0 = "../data/T0.json"
+
+        self.assertTrue(g.load_from_json(file0))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file1))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file2))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file3))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file4))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file5))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(fileT0))
+        g.plot_graph()
+
